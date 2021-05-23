7c43f1f3ec9b555b1312236c63e0b8d364fe6b8f
Bill Feng <bill.feng@yale.edu>
2021-05-23 00:23:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
   # your code 
-  array.length.times {
-    |index|
-    array[index]=array[index]**2
-  }
+  #array.length.times {
+  #  |index|
+  #  array[index]=array[index]**2
+  #}
   
   count=0
   while count<array.length {
